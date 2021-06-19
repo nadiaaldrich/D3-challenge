f341343c3cb66c403abcaf67723bd742a681f7e3
N Aldrich <naldrich@hs-27306.attlocal.net>
2021-06-18 23:25:09 -0500

    saving data

--- D3_Data_Journalism/StarterCode/assets/js/app.js
+++ D3_Data_Journalism/StarterCode/assets/js/app.js
@@ -6,7 +6,7 @@ var svgHeight = 600;
 var margin = {
     top: 40, 
     right: 40,
-    bottom: 40, 
+    bottom: 100, 
     left: 100, 
 
 };
@@ -26,11 +26,11 @@ var svg = d3
 // Append SVG group
 
 var chartGroup = svg.append("g")
-    .attr("transform", 'translate(${margin.left}, ${margin.top})');
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 // Get data from csv file
 
-d3.csv("/assets/data/data.csv").then(function(censusData){
+d3.csv("assets/data/data.csv").then(function(censusData){
     console.log('Actual Data:', censusData)
 
         censusData.forEach(function(data){
