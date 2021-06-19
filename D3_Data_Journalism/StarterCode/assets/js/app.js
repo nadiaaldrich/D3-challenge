f55563c25418061f1e02acfe162947b21430ea55
N Aldrich <naldrich@hs-27306.attlocal.net>
2021-06-18 23:08:33 -0500

    saving code

--- D3_Data_Journalism/StarterCode/assets/js/app.js
+++ D3_Data_Journalism/StarterCode/assets/js/app.js
@@ -1,1 +1,15 @@
-// @TODO: YOUR CODE HERE!
+// set SVG height and width
+
+var svgWidth = 960;
+var svgHeight = 600; 
+
+var margin = {
+    top: 40, 
+    right: 40,
+    bottom: 40, 
+    left: 100, 
+
+};
+
+var chartWidth = svgWidth - margin.left - margin.right;
+var chartHeight = svgHeight - margin.top - margin.bottom;
